87cc1c427dfbba424fa92fa6e3e19c82bd2756ae
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-06-04 00:43:50 +0530

    Update __main__.py

--- Newton/__main__.py
+++ Newton/__main__.py
@@ -6,7 +6,7 @@ import time
 import re
 import sys
 import traceback
-import ShuKurenaiXRoBot.modules.sql.users_sql as sql
+import Newton.modules.sql.users_sql as sql
 from sys import argv
 from typing import Optional
 from telegram import __version__ as peler
