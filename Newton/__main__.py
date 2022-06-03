b819119d1a74807f8cdf5f9451834ab1fa991363
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-06-04 01:37:19 +0530

    Update __main__.py

--- Newton/__main__.py
+++ Newton/__main__.py
@@ -383,17 +383,17 @@ def help_button(update, context):
 
 def shukurenai_about_callback(update, context):
     query = update.callback_query
-    if query.data == "shukurenai_":
+if query.data == "shukurenai_":
         query.message.edit_text(
             text="๏ I'm *Newton*, a powerful group management bot built to help you manage your group easily."
-            "\n乂 I can restrict users."
-            "\n乂 I can greet users with customizable welcome messages and even set a group's rules."
-            "\n乂 I have an advanced anti-flood system."
-            "\n乂 I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
-            "\n乂 I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
-            "\n乂 I check for admins' permissions before executing any command and more stuffs"
+            "\n• I can restrict users."
+            "\n• I can greet users with customizable welcome messages and even set a group's rules."
+            "\n• I have an advanced anti-flood system."
+            "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
+            "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
+            "\n• I check for admins' permissions before executing any command and more stuffs"
             "\n\n_Newton's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for Newton."
+            "\n\n Click on button bellow to get basic help for Newton.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
